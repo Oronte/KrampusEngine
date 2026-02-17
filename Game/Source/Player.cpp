a9f3bfd8238c0d0221952cf6bfab110ca45e4a16
Oronte <orontesoler06@gmail.com>
2026-02-17 16:59:54 +0100

    Update scene

--- Game/Source/Player.cpp
+++ Game/Source/Player.cpp
@@ -7,85 +7,50 @@
 Player::Player(Level* _level)
 	: Actor(_level)
 {
-	sprite = CreateComponent<SpriteComponent>(RectangleShapeData(FVector2(100.0f, 100.0f), "Character/JumpPack_CharacterSpriteSheet"));
-	collision = CreateComponent<CollisionComponent>();
-	//camera = CreateComponent<CameraComponent>();
-	animation = CreateComponent<AnimationComponent>();
-
-	animation->AddAnimation("Idle", AnimationData(4, 0.5f, SpriteData(IVector2(0, 32 * 0), IVector2(32))));
-	animation->AddAnimation("Move", AnimationData(4, 0.3f, SpriteData(IVector2(0, 32 * 1), IVector2(32))));
-	animation->AddAnimation("Dash", AnimationData(4, 0.3f, SpriteData(IVector2(0, 32 * 3), IVector2(32))));
-	animation->AddAnimation("Hit", AnimationData(4, 0.3f, SpriteData(IVector2(0, 32 * 4), IVector2(32)), false));
-	animation->AddAnimation("Death", AnimationData(4, 0.3f, SpriteData(IVector2(0, 32 * 5), IVector2(32)), false));
-	animation->AddAnimation("Sleep", AnimationData(4, 0.3f, SpriteData(IVector2(0, 32 * 6), IVector2(32))));
-
-	animation->StartAnimation();
+	sprite = CreateComponent<SpriteComponent>(RectangleShapeData(FVector2(1024.0f, 559.0f) * 0.3f, "KrampusEngineBanner"));
+	collision = CreateComponent<CollisionComponent>(CollisionChannel::All, CollisionChannel::All);
+	collision->BindCollisionResolution();
+	collision->useDebug = true;
 
 	inputs = CreateComponent<InputComponent>();
 	inputs->Bind(&GetWorld()->GetInputManager()->D.onPerform, this, &Player::MoveRight);
 	inputs->Bind(&GetWorld()->GetInputManager()->Q.onPerform, this, &Player::MoveLeft);
-	inputs->Bind(&GetWorld()->GetInputManager()->D.onRelease, this, &Player::Idle);
-	inputs->Bind(&GetWorld()->GetInputManager()->Q.onRelease, this, &Player::Idle);
-	inputs->Bind(&GetWorld()->GetInputManager()->LControl.onPress, this, &Player::Sleep);
-	inputs->Bind(&GetWorld()->GetInputManager()->H.onPress, this, &Player::Hit);
+	inputs->Bind(&GetWorld()->GetInputManager()->Z.onPerform, this, &Player::MoveUp);
+	inputs->Bind(&GetWorld()->GetInputManager()->S.onPerform, this, &Player::MoveDown);
+}
+
+Player::Player(Level* _level, CircleShapeData _data)
+	: Actor(_level)
+{
+	sprite = CreateComponent<SpriteComponent>(_data);
+	collision = CreateComponent<CollisionComponent>(CollisionChannel::All, CollisionChannel::All);
+	collision->useDebug = true;
+}
+
+Player::Player(Level* _level, RectangleShapeData _data)
+	: Actor(_level)
+{
+	sprite = CreateComponent<SpriteComponent>(_data);
+	collision = CreateComponent<CollisionComponent>(CollisionChannel::All, CollisionChannel::All);
+	collision->useDebug = true;
 }
 
 void Player::MoveLeft()
 {
 	transform.position += FVector2(-200.0f, 0) * GetWorld()->GetTimerManager()->GetDeltaTime();
-	if (state == State::Idle)
-	{
-		Move();
-		state = State::Moving;
-	}
-	transform.scale = FVector2(-1, 1);
 }
 
 void Player::MoveRight()
 {
 	transform.position += FVector2(200.0f, 0) * GetWorld()->GetTimerManager()->GetDeltaTime();
-	if (state == State::Idle)
-	{
-		Move();
-		state = State::Moving;
-	}
-	transform.scale = FVector2(1, 1);
 }
 
-void Player::Idle()
+void Player::MoveUp()
 {
-	state = State::Idle;
-	animation->SetCurrentAnimation("Idle");
-	animation->StartAnimation();
+	transform.position += FVector2(0, -200.0f) * GetWorld()->GetTimerManager()->GetDeltaTime();
 }
 
-void Player::Move()
+void Player::MoveDown()
 {
-	animation->SetCurrentAnimation("Move");
-	animation->StartAnimation();
-}
-
-void Player::Dash()
-{
-	animation->SetCurrentAnimation("Dash");
-	animation->StartAnimation();
-}
-
-void Player::Hit()
-{
-	animation->SetCurrentAnimation("Hit");
-	animation->StartAnimation();
-	GetLevel()->SpawnActor<BloodFX>()->transform.position = transform.position - FVector2(50.0f, 50.0f);
-}
-
-void Player::Death()
-{
-	animation->SetCurrentAnimation("Death");
-	animation->StartAnimation();
-}
-
-void Player::Sleep()
-{
-	animation->SetCurrentAnimation("Sleep");
-	animation->StartAnimation();
+	transform.position += FVector2(0, 200.0f) * GetWorld()->GetTimerManager()->GetDeltaTime();
 }
--- Game/Source/Player.h
+++ Game/Source/Player.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "Actors/Actor.h"
 #include "Components/SpriteComponent.h"
-#include "Components/AnimationComponent.h"
-#include "Components/CameraComponent.h"
 #include "Components/CollisionComponent.h"
 #include "Components/InputComponent.h"
 
@@ -11,29 +9,17 @@ using namespace Krampus;
 class Player : public Actor
 {
 	SpriteComponent* sprite = nullptr;
-	AnimationComponent* animation = nullptr;
-	CameraComponent* camera = nullptr;
 	CollisionComponent* collision = nullptr;
 	InputComponent* inputs = nullptr;
 
-	enum class State
-	{
-		Moving,
-		Idle
-	};
-	State state = State::Idle;
-
 public:
 	Player(Level* _level);
+	Player(Level* _level, CircleShapeData _data);
+	Player(Level* _level, RectangleShapeData _data);
 
 	void MoveLeft();
 	void MoveRight();
-
-	void Idle();
-	void Move();
-	void Dash();
-	void Hit();
-	void Death();
-	void Sleep();
+	void MoveUp();
+	void MoveDown();
 };
 
